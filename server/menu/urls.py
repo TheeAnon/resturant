681bceb510a152e8e89a756050c6c4c708a987e2
unknown <hazardovic1@gmail.com>
2024-06-20 13:34:32 +0300

    hdj

--- server/menu/urls.py
+++ server/menu/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('item/<int:id>/', views.menu_item, name='menu_item'),
     path('categories/', views.categories, name='categories'),
     path('categorized/', views.categorized_menu_items, name='categorized_items'),
-    path('create/item/', views.create_menu_item, name='create item'),
+    path('create/item/', views.create_menu_item, name='create menu item'),
     path('create/category/', views.create_category, name='create category'),
 ]
